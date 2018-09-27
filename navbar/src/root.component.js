7b916f64acb9f00a833827de36b2599a2b4a0622
Justin McMurdie <mcmurdiej@gmail.com>
2018-09-27 16:05:56 -0600

    More navigation setup

--- navbar/src/root.component.js
+++ navbar/src/root.component.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Scoped } from 'kremling'
 import styles from './root.krem.css'
+import { links } from './root.helper.js'
 
 export default class Root extends React.Component {
   render () {
     return (
       <Scoped postcss={styles}>
         <div className='root'>
-          Hello there
+          {
+            links.map((link) => {
+              return (
+                <a
+                  className='primary-navigation-link'
+                  href={link.href}
+                >
+                  {link.name}
+                </a>
+              )
+            })
+          }
         </div>
       </Scoped>
     )
---
+++ navbar/src/root.helper.js
@@ -0,0 +1,10 @@
+export const links = [
+  {
+    name: 'App 1',
+    href: '/#/app1'
+  },
+  {
+    name: 'App 2',
+    href: '/#/app2'
+  }
+]
