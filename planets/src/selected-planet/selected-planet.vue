3c15b4da7093c151b39f6003c0108257c893eb57
Justin McMurdie <mcmurdiej@gmail.com>
2018-10-05 13:37:44 -0600

    Adding more content to the selected planet

--- planets/src/selected-planet/selected-planet.vue
+++ planets/src/selected-planet/selected-planet.vue
@@ -1,6 +1,58 @@
 <template>
-  <div class="selectedPlanet" v-if='!!selectedPlanet'>
-    {{selectedPlanet.name}}
+  <div>
+    <div class="selectedPlanet" v-if='!!selectedPlanet'>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Name
+        </div>
+        <div class='attribute'>
+          {{selectedPlanet.name}}
+        </div>
+      </div>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Climate
+        </div>
+        <div class='attribute'>
+          {{selectedPlanet.climate}}
+        </div>
+      </div>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Diameter
+        </div>
+        <div class='attribute'>
+          {{ selectedPlanet.diameter | formatDiameter }}
+        </div>
+      </div>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Gravity
+        </div>
+        <div class='attribute'>
+          {{selectedPlanet.gravity}}
+        </div>
+      </div>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Population
+        </div>
+        <div class='attribute'>
+          {{ selectedPlanet.population | formatPopulation }}
+        </div>
+      </div>
+      <div class='planetAttribute'>
+        <div class='attributeTitle'>
+          Terrain
+        </div>
+        <div class='attribute'>
+          {{selectedPlanet.terrain}}
+        </div>
+      </div>
+    </div>
+    <div class="selectedPlanet" v-if='!selectedPlanet'>
+      No planet selected
+    </div>
   </div>
 </template>
 
@@ -8,23 +60,71 @@
 export default {
   props: {
     selectedPlanet: Object
+  },
+  filters: {
+    formatPopulation: function (value) {
+      if(!value) {
+        return ''
+      } else {
+        const parsedValue = parseInt(value)
+        if (isNaN(parsedValue)) {
+          return value
+        } else {
+          return abbrNum(value)
+        }
+      }
+    },
+    formatDiameter: function (value) {
+      return `${value} Kilometers (${value * 0.621371} Miles)`
+    }
   }
+}
+// copied from stack overflow
+function abbrNum (number, decPlaces = 2) {
+  // 2 decimal places => 100, 3 => 1000, etc
+  decPlaces = Math.pow(10, decPlaces)
+
+  // Enumerate number abbreviations
+  const abbrev = [ "k", "m", "b", "t" ]
+  let newNumber = number
+
+  // Go through the array backwards, so we do the largest first
+  for (let i=abbrev.length-1; i>=0; i--) {
+
+    // Convert array index to "1000", "1000000", etc
+    const size = Math.pow(10,(i+1)*3)
+
+    // If the number is bigger or equal do the abbreviation
+    if(size <= newNumber) {
+      // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+      // This gives us nice rounding to a particular decimal place.
+      newNumber = Math.round(number*decPlaces/size)/decPlaces
+
+      // Handle special case where we round up to the next abbreviation
+      if((newNumber == 1000) && (i < abbrev.length - 1)) {
+        newNumber = 1
+        i++
+      }
+
+      // Add the letter for the abbreviation
+      newNumber += abbrev[i]
+
+      // We are done... stop
+      break
+    }
+  }
+  return newNumber
 }
 </script>
 
 <style scoped>
-.rootDiv {
+.planetAttribute {
   display: flex;
 }
 
-.left {
-  padding: 24px;
-  width: 30%;
-}
-
-.right {
-  width: 70%;
-  border-left: 2px solid var(--white);
-  padding: 24px;
+.attributeTitle {
+  font-weight: bold;
+  padding-right: 16px;
+  width: 150px;
 }
 </style>
