9ed4ee6bb445bb4c94defa39bb292e5a32bb282e
Justin McMurdie <mcmurdiej@gmail.com>
2018-12-20 14:21:46 -0700

    Adding a common api tool

--- people/src/utils/api.js
+++ people/src/utils/api.js
@@ -1,49 +1,23 @@
-import axios from 'axios'
-import { pipe, from } from 'rxjs'
-import { pluck } from 'rxjs/operators'
-
-const axiosInstance = axios.create({
-  baseURL: 'https://swapi.co/api/',
-  timeout: 20000,
-})
+import fetchWithCache from 'fetcher!sofe'
 
 export function getPeople(pageNum = 1) {
-  return from(
-    axiosInstance.get(
-      `people/?page=${pageNum}`
-    )
-  ).pipe(
-    pluck('data')
-  )
+  return fetchWithCache(`people/?page=${pageNum}`)
 }
 
 export function getPlanet(id) {
-  return from(
-    axiosInstance.get(
-      `planets/${id}/`
-    )
-  ).pipe(
-    pluck('data')
+  return fetchWithCache(
+    `planets/${id}/`
   )
 }
 
 export function getFilm(filmId) {
-  return from(
-    axiosInstance.get(
-      `films/${filmId}`
-    )
-  ).pipe(
-    pluck('data')
+  return fetchWithCache(
+    `films/${filmId}`
   )
 }
 
 export function getFilms() {
-  return from(
-    axiosInstance.get(
-      `films`
-    )
-  ).pipe(
-    pluck('data')
+  return fetchWithCache(
+    `films`
   )
-
 }
